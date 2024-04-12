842de47ead6da296a3a09e8090abc9c2c90c7ce0
Vladislav0904 <vladislav0904@gmail.com>
2024-04-12 18:16:20 +0300

    KAN-5 Created function prototype that returns list of room and time when it is booked (if booked)

--- timetable_util/main.py
+++ timetable_util/main.py
@@ -1,6 +1,5 @@
-from requests import get
-from pprint import pprint
 import json
+from requests import get
 
 req = get("https://ruz.spbstu.ru/api/v1/ruz/buildings/")
 data_buildings = {}
@@ -20,3 +19,27 @@ for i in data_buildings.keys():
 
 with open('data_rooms.json', 'w', encoding='utf-8') as f:
     json.dump(data_rooms, f, ensure_ascii=False, indent=4, sort_keys=True)
+
+
+def get_occupied_rooms(building):
+    with open('data_buildings.json', 'r',
+              encoding='utf-8') as f:  # Временные json с айдишниками и именами
+        buildings = json.load(f)  # аудиторий/корпусов
+    with open('data_rooms.json', 'r', encoding='utf-8') as f:
+        rooms = json.load(f)
+
+    building = buildings[building]
+    answer = {"rooms": {}}
+    for room in rooms[str(building)]:
+        request = get(
+            f"https://ruz.spbstu.ru/api/v1/ruz/buildings/{building}/rooms/{rooms[str(building)][room]}/scheduler")
+        try:
+            answer["rooms"][rooms[str(building)][room]] = []
+            for lessons in request.json()['days']:
+                temp = {"time_start": lessons["lessons"][0]["time_start"],
+                        "time_end": lessons["lessons"][0]["time_end"],
+                        "date": lessons["date"]}
+                answer["rooms"][rooms[str(building)][room]].append(temp)
+        except KeyError:
+            print(f"Аудитория {rooms[str(building)][room]} вне расписания")
+    return answer
