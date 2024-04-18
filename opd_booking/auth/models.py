50e2eec8dd9a4f614f6682a5b5be42d6c0b109db
nikitakurtash <nikitakurtash@gmail.com>
2024-04-18 23:22:30 +0300

    sketches of db

--- opd_booking/auth/models.py
+++ opd_booking/auth/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Students(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=30)
+    email = models.EmailField()
+    access = models.IntegerField(max_length=5)
+    is_reserved = models.BooleanField(default=False)
+    reserved_audience_id = models.IntegerField(max_length=30)
+    reserved_audience_slot = models.IntegerField(max_length=30)
+def __str__(self):
+    return f"User {self.username} ({self.email})"
