bc09d96d2a807ac3e0e0e4244aac7d8830e4f644
BADBOYKILLA <33057075+Vladislav0904@users.noreply.github.com>
2024-04-20 20:50:15 +0300

    KAN-10 Added Booking model (#10)

--- opd_booking/timetable/admin.py
+++ opd_booking/timetable/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Audience, Building
+from .models import Audience, Building, Booking
 # Register your models here.
 
 admin.site.register(Audience)
 admin.site.register(Building)
+admin.site.register(Booking)
--- opd_booking/timetable/serializers.py
+++ opd_booking/timetable/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Building, Audience
+from .models import Building, Audience, Booking
 
 
 class BuildingSerializer(serializers.ModelSerializer):
@@ -13,3 +13,9 @@ class AudienceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Audience
         fields = ['interior_id', 'name', 'building', 'is_available_for_book', 'type']
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ['audience', 'time', 'date', 'ordered_by']
